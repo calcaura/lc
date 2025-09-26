550bedfa24d56434403e24837871c0d18b7eaf0f
Ghenadie Calcaura <calcaura@gmail.com>
2025-09-26 14:37:25 +0000

    [template] Add log sample to templates

--- problemset/_template/test.cpp
+++ problemset/_template/test.cpp
@@ -7,9 +7,16 @@
 // C++ STL
 #include <algorithm>
 #include <iostream>
+#include <lc_libs/simple_log.hpp>
 #include <vector>
 
+namespace log = lc_libs::log;
+
 TEST(TemplateNameTest, BasicOperations) {
+  log::debug("Debug message: {}", 42);
+  log::info("Info message: {}", 3.14);
+  log::warn("Warning message: {}", "be careful!");
+  log::error("Error message: {}", "something went wrong!");
   ASSERT_EQ(1, 1);
   ASSERT_GE(2, 1);
   ASSERT_LE(1, 2);
