316518e517aab888b999a552080879a92e8616ca
Ghenadie Calcaura <60554549+calcaura@users.noreply.github.com>
2025-09-26 10:24:26 +0200

    Uniform libs (#6)
    * [libs] Move libs (used for many problems) into dedicated folder
    
    * [libs] Move list_node

--- problemset/0021.MergeTwoSortedLists/solution.hpp
+++ problemset/0021.MergeTwoSortedLists/solution.hpp
@@ -4,63 +4,9 @@
 
 #include <initializer_list>
 #include <iostream>
+#include <lc_libs/list_node.hpp>
 
-struct ListNode {
-  int val;
-  ListNode* next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode* next) : val(x), next(next) {}
-  ListNode(std::initializer_list<int> vals) : next(nullptr) {
-    if (vals.size() == 0) {
-      // Empty list
-      val = 0;
-      next = nullptr;
-      return;
-    }
-    auto it = vals.begin();
-    val = *it;
-    ++it;
-    ListNode* crt{this};
-    for (; it != vals.end(); ++it) {
-      crt->next = new ListNode(*it);
-      crt = crt->next;
-    }
-  }
-
-  bool operator==(const ListNode& other) const {
-    const ListNode* l1{this};
-    const ListNode* l2{&other};
-    while (l1 && l2) {
-      if (l1->val != l2->val) {
-        return false;
-      }
-      l1 = l1->next;
-      l2 = l2->next;
-    }
-    return l1 == nullptr && l2 == nullptr;
-  }
-
-  bool operator==(const std::initializer_list<int>& vals) const {
-    auto it = vals.begin();
-    const ListNode* crt{this};
-    for (; it != vals.end() && crt; ++it, crt = crt->next) {
-      if (crt->val != *it) {
-        return false;
-      }
-    }
-    return it == vals.end() && crt == nullptr;
-  }
-
-  ListNode* unlink() {
-    ListNode* next_node = next;
-    next = nullptr;
-    return next_node;
-  }
-
-  // Destructor to free the entire list with the exception of the head node
-  ~ListNode() { delete next; }
-};
+using ListNode = lc_libs::ListNodeBase<int>;
 
 class Solution {
  public:
