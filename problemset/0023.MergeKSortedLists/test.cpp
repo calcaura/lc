0d56afe487c50a5c64d478ddb49902f71a3ea1ca
Ghenadie Calcaura <60554549+calcaura@users.noreply.github.com>
2025-10-02 15:29:24 +0200

    Fix ListNode std::format (#21)

--- problemset/0023.MergeKSortedLists/test.cpp
+++ problemset/0023.MergeKSortedLists/test.cpp
@@ -7,20 +7,38 @@
 // C++ STL
 #include <algorithm>
 #include <iostream>
+#include <lc_libs/simple_log.hpp>
 #include <vector>
 
-TEST(TemplateNameTest, Null) {
+namespace log = lc_libs::log;
+
+TEST(ListNode, Printer) {
+  ListNode list{1, 2, 3, 4, 5};
+  EXPECT_EQ(std::format("[{}]", list), "[1, 2, 3, 4, 5]");
+
+  auto adv = list.advance(2);
+  EXPECT_EQ(std::format("[{}]", adv), "[3, 4, 5]");
+
+  EXPECT_EQ(std::format("[{}]", list.view("->")), "[1->2->3->4->5]");
+  auto view = ListNode::view(&list, list.advance(3), "|");
+  EXPECT_EQ(std::format("/{}/", view), "/1|2|3|4/");
+}
+
+TEST(MergeKSortedLists, Null) {
   Solution s;
   std::vector<ListNode*> lists;
   EXPECT_EQ(s.mergeKLists(lists), nullptr);
 }
 
-TEST(TemplateNameTest, ThreeLists) {
+TEST(MergeKSortedLists, ThreeLists) {
   Solution s;
   std::vector<ListNode*> lists;
   lists.push_back(new ListNode({1, 4, 5}));
   lists.push_back(new ListNode({1, 3, 4}));
   lists.push_back(new ListNode({2, 6}));
+
+  log::debug("Merging lists: [{}]; [{}]; [{}]", lists[0], lists[1], lists[2]);
+
   ListNode* result = s.mergeKLists(lists);
   EXPECT_EQ(*result, (ListNode{1, 1, 2, 3, 4, 4, 5, 6}));
   delete result;
